1478accfc3be9d8898e7604e7b93f2276666a397
Antonio Ganea <antonio.ganea@yahoo.com>
2018-07-26 13:47:05 +0300

    Trade/Inventory views

--- www/js/index.js
+++ www/js/index.js
@@ -45,7 +45,75 @@ var app = {
 
 app.initialize();
 
-document.getElementById('trade')
-.addEventListener('click', function (event) {
-	document.getElementById('trade').innerHTML = "Trade1";
+
+
+
+var tradeButton = document.getElementById('trade');
+var inventoryButton = document.getElementById('inventory');
+inventoryButton.classList.remove("btn-primary");
+inventoryButton.classList.add("btn-secondary");
+
+var mainBoard = document.getElementById('main-board');
+var offerList = document.getElementById('offer-list');
+var inventoryList = document.getElementById('inventory-list');
+
+var current_display;
+function switchDisplay( display ){
+	if ( display === 'trade' ){
+		if ( current_display !== 'trade' ){
+			current_display = display;
+			offerList.setAttribute('style', 'display:block;');
+			inventoryList.setAttribute('style', 'display:none;');
+			tradeButton.classList.remove("btn-secondary");
+			tradeButton.classList.add("btn-primary");
+			inventoryButton.classList.remove("btn-primary");
+			inventoryButton.classList.add("btn-secondary");
+		}
+	}else{
+		if ( display === 'inventory' ){
+			if ( current_display !== 'inventory' ){
+				current_display = display;
+				offerList.setAttribute('style', 'display:none;');
+				inventoryList.setAttribute('style', 'display:block;');
+				tradeButton.classList.remove("btn-primary");
+				tradeButton.classList.add("btn-secondary");
+				inventoryButton.classList.remove("btn-secondary");
+				inventoryButton.classList.add("btn-primary");
+			}
+		}
+	}
+}
+switchDisplay('trade');
+
+tradeButton.addEventListener('click', function( event ){
+	switchDisplay('trade');
 });
+
+inventoryButton.addEventListener('click', function( event ){
+	switchDisplay('inventory');
+	//addInventoryItem({itemID: 10,game:"GTA 5 MP",image:"mustang.png",displayName:"Ford Mustang GT", name:"mustang", serverName:"Nitro GTA 5 Romania"});
+});
+
+
+
+function addInventoryItem( itemData ){
+	var iDiv = document.createElement('div');
+	iDiv.id = 'item-' + itemData.itemID;
+	iDiv.className = 'block';
+	iDiv.innerHTML=`<div class="media text-muted pt-3">
+	  <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+		<strong class="d-block text-gray-dark">` + itemData.game + `</strong>
+		
+		<img src="img/` + itemData.image + `" style="height:64px;padding-right:10px">
+		<strong>Item : </strong>` + itemData.displayName + `<br>
+		<strong>Server : </strong>` + itemData.serverName + `<br>
+		<button class="btn btn-primary my-1 box-shadow float-right">Sell Now</button>
+	  </p>
+	  </div>`;
+	
+	inventoryList.appendChild(iDiv);
+}
+
+
+
+addInventoryItem({itemID: 10,game:"GTA 5 MP",image:"mustang.png",displayName:"Ford Mustang GT", name:"mustang", serverName:"Nitro GTA 5 Romania"});
